8096a57ea5e0a42b577d7a54bd65d090fcc5c8ec
Richard Wilson <rcwilson88@gmail.com>
2019-10-22 15:12:23 -0400

    Created Styling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import main from "./js/main"
-
 main()
+
+let playerName = document.getElementById("player_name")
+playerName.innerHTML = prompt("What's your name you sick piece of shit?", "Ivana Lose")
+playerName.innerHTML = "RICHARD BITCH"
