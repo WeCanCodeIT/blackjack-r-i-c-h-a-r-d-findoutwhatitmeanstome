a2fb2ff85a22db16cda32fcce97898c30d845488
richard wilson <rcwilson88@gmail.com>
2019-10-27 03:27:21 -0400

    Game logic completed. Style completed.

--- src/index.js
+++ src/index.js
@@ -6,24 +6,40 @@ const Dom = require("./utils/Dom");
 
 
 
-let playerName = document.getElementById("player_name");
+let playerName = document.getElementById("player-name");
 let dealerScore = document.querySelector(".dealer-score");
 let playerScore = document.querySelector(".player-score");
 let hitButton = document.querySelector(".hit");
 let standButton = document.querySelector(".stand");
+let doubleButton = document.querySelector(".double");
 let newGameButton = document.querySelector(".new");
 let gameStatus = document.querySelector(".status");
-    playerName.innerHTML = "Richard, Bitch";
-
-    hitButton.disabled = true;
-    standButton.disabled = true;
+let playerTokens = document.getElementById("counter")
+    playerName.innerHTML = prompt("What's your name?", "Jimmy Olivia Smith")
+    gameStatus.innerHTML = `Hit the "New Round" button, ${playerName.innerHTML}`;
+    refreshPlayerTokens();
+    endRoundButtons();
 
 let dealerHand 
 let userHand 
 
 
 
+
+
+
+
 // BUTTONS -------------------
+
+doubleButton.addEventListener("click", () => {
+    userDoubles();
+        refreshPlayerTokens();
+        if (singleDeckGame.isUserBust()) {
+
+            endRound();
+            getWinner(singleDeckGame.outcome());
+            }
+});
 
 hitButton.addEventListener("click", () => {
 
@@ -32,10 +48,9 @@ hitButton.addEventListener("click", () => {
 
             endRound();
             getWinner(singleDeckGame.outcome());
+            
             }
 });
-
-
 standButton.addEventListener("click", () =>{
     singleDeckGame.standUser();
 
@@ -45,67 +60,143 @@ standButton.addEventListener("click", () =>{
         }
     getWinner(singleDeckGame.outcome());      
 })
-
-    
-
-
 newGameButton.addEventListener("click", () => {
-    newGameButton.disabled = true;
-    hitButton.disabled = false;
-    standButton.disabled = false;
-    gameStatus.textContent = "";
-
-    singleDeckGame.resetPlayers();
-    singleDeckGame.deal();    
-
-    dealerHand = singleDeckGame.getDealerHand();
-    userHand = singleDeckGame.getUserHand();
-
-    dealerScore.textContent = singleDeckGame.evaluateDealer();
-    playerScore.textContent = singleDeckGame.evaluateUser();
-
-    document.querySelector(".user").innerHTML = "";
-    document.querySelector(".dealer").innerHTML = "";
-
-    Dom.renderCards(userHand.getCards(), document.querySelector(".user"));
-    Dom.renderCards(dealerHand.getCards(), document.querySelector(".dealer"));
+    startBettingRound();
 });
 
+
+
+function startNewGameRound() {
+    newGameButtons();
+    displayToConsole(`${playerName.innerHTML} vs. Dealer`);
+    resetGame();
+    clearTableHandAreas();
+    dealHands();
+}
+
+function displayToConsole(input) {
+    gameStatus.textContent = input;
+}
+
+function resetGame() {
+    singleDeckGame.resetPlayers();
+    singleDeckGame.deal();
+}
+
+function clearScores() {
+    dealerScore.textContent = 0;
+    playerScore.textContent = 0;
+}
+
+function dealHands() {
+    dealerHand = singleDeckGame.getDealerHand();
+    userHand = singleDeckGame.getUserHand();
+    Dom.renderCards(userHand.getCards(), document.querySelector(".user"));
+    Dom.renderAnonCard(document.querySelector(".dealer"));
+    Dom.renderCards([dealerHand.getCards()[1]], document.querySelector(".dealer"));
+}
+
+function clearTableHandAreas() {
+    document.querySelector(".user").innerHTML = "";
+    document.querySelector(".dealer").innerHTML = "";
+}
+
+function startBettingRound() {
+    displayToConsole("Place Your Bet");
+    clearScores();
+    clearTableHandAreas()
+    newGameButton.disabled = true;
+    Dom.renderTokens([10, 25, 100], document.querySelector(".dealer"));
+    document.querySelector(".token.token-10").addEventListener("click", () =>{
+        singleDeckGame.receiveAnte(10);
+        refreshPlayerTokens()
+        startNewGameRound();
+        getPlayerScore();
+    })
+    document.querySelector(".token.token-25").addEventListener("click", () =>{
+        singleDeckGame.receiveAnte(25);
+        refreshPlayerTokens()
+        startNewGameRound();
+        getPlayerScore();
+    })
+    document.querySelector(".token.token-100").addEventListener("click", () =>{
+        singleDeckGame.receiveAnte(100);
+        refreshPlayerTokens()
+        startNewGameRound();
+        getPlayerScore();
+    })
+}
+
+function refreshPlayerTokens() {
+    playerTokens.innerHTML = singleDeckGame.getUserChips();
+}
+
+function newGameButtons() {
+    newGameButton.disabled = true;
+    doubleButton.disabled = false;
+    hitButton.disabled = false;
+    standButton.disabled = false;
+}
+
 function endRound() {
-    hitButton.disabled = true;
-    standButton.disabled = true;
-    newGameButton.disabled = false;
+    endRoundButtons();
+    document.querySelector(".dealer").innerHTML = "";
+    Dom.renderCards(dealerHand.getCards(), document.querySelector(".dealer"));
+    getDealerScore();
+    singleDeckGame.resetAnte();
+
+}
+
+function getDealerScore() {
+    dealerScore.innerHTML = singleDeckGame.evaluateDealer();
+}
+function getPlayerScore() {
+    playerScore.innerHTML = singleDeckGame.evaluateUser();
 }
 
 function getWinner(outcome) {
+    singleDeckGame.outcome();
+    
     switch (outcome) {
         case 0:
-            hitButton.disabled = true;
-            standButton.disabled = true;
-            newGameButton.disabled = false;
+            endRoundButtons();
             gameStatus.textContent = "Dealer Wins!";
 
             break;
         case 1: 
-            hitButton.disabled = true;
-            standButton.disabled = true;
-            newGameButton.disabled = false;
+            endRoundButtons();
             gameStatus.textContent = "Draw!";
 
             break;
         case 2:
-            hitButton.disabled = true;
-            standButton.disabled = true;
-            newGameButton.disabled = false;
+            endRoundButtons();
+            singleDeckGame.userWin();
             gameStatus.textContent = "You Win!";
-}   
+        }   ;
+        refreshPlayerTokens()
+        singleDeckGame.resetAnte();
 }
 
+function endRoundButtons() {
+    hitButton.disabled = true;
+    standButton.disabled = true;
+    doubleButton.disabled = true;
+    newGameButton.disabled = false;
+}
+
+function userDoubles() {
+    singleDeckGame.doubleUser();
+    document.querySelector(".user").innerHTML = "";
+    Dom.renderCards(userHand.getCards(), document.querySelector(".user"));
+    getPlayerScore();
+    
+};
 function userHits() {
     singleDeckGame.hitUser();
     document.querySelector(".user").innerHTML = "";
     Dom.renderCards(userHand.getCards(), document.querySelector(".user"));
-    playerScore.innerHTML = singleDeckGame.evaluateUser();
+    getPlayerScore();
+    
 };
 function dealerHits() {
     document.querySelector(".dealer").innerHTML = "";
--- src/utils/Dom.js
+++ src/utils/Dom.js
@@ -26,5 +26,62 @@ module.exports = {
             containerElement.append(this.generateCard(card));
         })
     },
-  
+    generateAnonCard() {
+        const playingCard = document.createElement("section");
+        playingCard.classList.add("playing-card");
+        
+        const valueContainer = document.createElement("section");
+        valueContainer.classList.add("card-inner-line_anon");
+
+        playingCard.append(valueContainer);
+
+        return playingCard;
+    },
+    renderAnonCard(containerElement){
+        containerElement.append(this.generateAnonCard());
+    },
+   
+    generateToken(token) {
+        const bettingToken = document.createElement("section");
+        bettingToken.classList.add("token");
+        
+        
+        const valueContainer = document.createElement("section");
+        valueContainer.classList.add("token-value-container");
+
+        const value = document.createElement("span");
+        value.classList.add("token-value")
+
+        switch(token){
+            case 10:
+                bettingToken.classList.add("token-10");
+                valueContainer.classList.add("token-10");
+                value.innerText = "10";
+                break;
+
+            case 25:
+                bettingToken.classList.add("token-25");
+                valueContainer.classList.add("token-25");
+                value.innerText = "25";
+                break;
+            case 100:
+                bettingToken.classList.add("token-100");
+                valueContainer.classList.add("token-100");
+                value.innerText = "100";
+                break;
+                
+        }
+
+        valueContainer.append(value);
+        bettingToken.append(valueContainer);
+
+        return bettingToken;
+    },
+
+    renderTokens(tokensArray, containerElement) {
+        tokensArray.forEach(token => {
+            containerElement.append(this.generateToken(token));
+        })
+    }
+
 };
